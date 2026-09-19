ba05771b0afecd67879e2368feac6ee2390be4ab
LEXavier <lesxavier@outlook.com>
2026-09-19 02:27:51 -0300

    Adicionando questão 5

--- lista2/operacoes.py
+++ lista2/operacoes.py
@@ -9,46 +9,7 @@
 
 
 
-# def inverte_fila_com_duas_filas(fila):
-#     # Questão 5.b
-#     #
 
-#     quantidade = fila.get_quantidade()
-
-#     fila_aux1 = Fila(quantidade)
-#     fila_aux2 = Fila(quantidade)
-
-#     for i in range(quantidade):
-
-#         dado1 = fila.desenfileira()
-
-#         fila_aux1.enfileira(dado1)
-
-#         for j in range(i):
-#             dado2 = fila_aux2.desenfileira()
-#             fila_aux1.enfileira(dado2)
-
-#         for j in range(i+1):
-#             dado2 = fila_aux1.desenfileira()
-#             fila_aux2.enfileira(dado2)
-#     return fila_aux2
-
-# def inverte_fila_com_pilha(fila):
-#     # Questão 5.a
-#     # 
-    
-#     quantidade = fila.get_quantidade()
-#     pilha = Pilha(quantidade)
-
-#     for i in range(quantidade):
-#         dado = fila.desenfileira()
-#         pilha.empilha(dado)
-
-#     for i in range(quantidade):
-#         dado = pilha.desempilha()
-#         fila.enfileira(dado)
-
-#     return fila
 
 # def eh_operador(char):
 #     return (char == '-') or (char == '+') or (char == '*') or (char == '/')
---
+++ lista2/questao_cinco.py
@@ -0,0 +1,43 @@
+from fila import FilaStr as Fila
+from pilha import PilhaStr as Pilha
+
+def inverte_fila_com_duas_filas(fila:Fila) -> Fila:
+    # Questão 5.b
+    #
+
+    quantidade = fila.tam
+
+    fila_aux1 = Fila(quantidade)
+    fila_aux2 = Fila(quantidade)
+
+    for i in range(quantidade):
+
+        dado1 = fila.desenfileira()
+
+        fila_aux1.enfileira(dado1)
+
+        for j in range(i):
+            dado2 = fila_aux2.desenfileira()
+            fila_aux1.enfileira(dado2)
+
+        for j in range(i+1):
+            dado2 = fila_aux1.desenfileira()
+            fila_aux2.enfileira(dado2)
+    return fila_aux2
+
+def inverte_fila_com_pilha(fila:Fila) -> Fila:
+    # Questão 5.a
+    # 
+    
+    quantidade = fila.tam
+    pilha = Pilha(quantidade)
+
+    for i in range(quantidade):
+        dado = fila.desenfileira()
+        pilha.empilha(dado)
+
+    for i in range(quantidade):
+        dado = pilha.desempilha()
+        fila.enfileira(dado)
+
+    return fila
